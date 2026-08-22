cbea048bdc725517ae13a8aa524222d3f7d40262
scientistofeverything <nbunny2010@gmail.com>
2026-08-22 11:52:44 -0400

    THE PLAYER IS HERE

--- despairpy/main.js
+++ despairpy/main.js
@@ -1,4 +1,36 @@
+// Canvas Setup
+
 let canvas = document.getElementById("game_window");
 let ctx = canvas.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 1200, 1600);
+ctx.imageSmoothingEnabled = false;
+
+// Drawing Functions
+
+function clear (context, color) {
+    context.fillStyle = color;
+    context.fillRect(0, 0, 1200, 1600);
+}
+
+function drawSprite (context, sprite, x, y, scale) {
+    context.drawImage(sprite, x, y, sprite.width * scale, sprite.height * scale);
+}
+
+// Game Setup
+
+clear(ctx, "#bfbfff");
+
+let gameObjects = [];
+
+let player = {
+    texture: document.getElementById("spr_0"),
+    x: 0,
+    y: 0
+}
+
+gameObjects.push(player);
+
+window.setInterval(function () {
+    for (let obj of gameObjects) {
+        drawSprite(ctx, obj.texture, obj.x, obj.y, 2);
+    }
+}, 50);
